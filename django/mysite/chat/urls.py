e0a1e57b1a88bc24317f3dbd552f40b9460d5da2
ekinare <57627144+ekinare@users.noreply.github.com>
2021-03-30 17:49:55 +0900

    update django
    webcam으로 mediapipe javascript 돌리는부분 추가

--- django/mysite/chat/urls.py
+++ django/mysite/chat/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^webcam/$', views.webcam, name='webcam'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
