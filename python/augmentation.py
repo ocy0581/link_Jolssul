1e5df85c8bbf32851a654b51400cb950c3d455c0
ekinare <57627144+ekinare@users.noreply.github.com>
2021-05-12 17:57:34 +0900

    update augmentation and transformer_model

--- python/augmentation.py
+++ python/augmentation.py
@@ -4,33 +4,26 @@ import math
 
 class DataAugmentation(object):
     '''
-    독립적으로 진행 
-    손1개로 일단 제작중
-
-------------------------------------------
-    eps의 확률로 좌우 중에 scale만큼 이동
-    eps의 확률로 상하 중에 scale만큼 이동
     
- -------------------------------------------   구현완료
-
-    구현중
-    1/4의 확률로 x축 축소 
-    1/4의 확률로 y축 축소
 
     '''
     def __init__(self, sample_data,max_scale=0.05,eps = 0.5,device = 'cpu' ):
         self.eps = eps
+
         if(eps > 1 ):
             self.eps = 1
-
-
+        elif eps < 0 :
+            self.eps = 0
         self.data_shape = sample_data.shape
-        self.x = torch.FloatTensor([1,0,0]*self.data_shape[0]).repeat(self.data_shape[0]*self.data_shape[1]
-        ).reshape(self.data_shape).to(device)
-        self.y = torch.FloatTensor([0,1,0]*self.data_shape[0]).repeat(self.data_shape[0]*self.data_shape[1]
-        ).reshape(self.data_shape).to(device)
-        self.z = torch.FloatTensor([0,0,1]*self.data_shape[0]).repeat(self.data_shape[0]*self.data_shape[1]
-        ).reshape(self.data_shape).to(device)
+
+        self.x = torch.FloatTensor([1,0,0]*int(self.data_shape[2]/3)).repeat(self.data_shape[0]*self.data_shape[1]
+                                                                        ).reshape(self.data_shape).to(device)
+
+        self.y = torch.FloatTensor([0,1,0]*int(self.data_shape[2]/3)).repeat(self.data_shape[0]*self.data_shape[1]
+                                                                        ).reshape(self.data_shape).to(device)
+
+        self.z = torch.FloatTensor([0,0,1]*int(self.data_shape[2]/3)).repeat(self.data_shape[0]*self.data_shape[1]
+                                                                        ).reshape(self.data_shape).to(device)
 
         self.scale = max_scale
 
@@ -39,27 +32,31 @@ class DataAugmentation(object):
         # x축 증식
         if(random.random() < self.eps):
             data += self.x * (random.random()*self.scale*2 - self.scale)
-            # print('x증식 =======================\n{}\n============'.format(data[0,0]))
         # y축 증식
         if(random.random() < self.eps):
             data += self.y * (random.random()*self.scale*2 - self.scale)
-            # print('y증식=======================\n{}\n============'.format(data[0,0]))
+
+        # z축 증식
+        if(random.random() < self.eps):
+            data += self.z * (random.random()*self.scale*2 - self.scale)/2
+
+
+
+
 
         # x축 회전 및 중앙정렬
         if(random.random() < self.eps):
             rand = random.random()*self.scale*2+(1-self.scale)
             data = data.mul(self.x*rand+self.y+self.z) + self.x*(1-rand)/2
-            # print(rand)
-            # print('x,회전=======================\n{}\n============'.format( data[0,0]))
         
+
         # y축 회전 및 중앙정렬
         if(random.random() < self.eps):
             rand = random.random()*self.scale*2+(1-self.scale)
             data = data.mul(self.x+self.y*rand+self.z) + self.y*(1-rand)/2
-            # print(rand)
-            # print('y,회전=======================\n{}\n============'.format(data[0,0]))
-        # tmp = x + data + z
-        # print(tmp[0][0])
+
+
+        
         return data
 
     
