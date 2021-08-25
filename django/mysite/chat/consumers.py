7fc64c7649e72d51d338882feb9e1cc95b66614e
ocy0581 <ocy0581@naver.com>
2021-08-25 18:24:12 +0900

    modified:   .gitignore 	modified:   django/mysite/chat/consumers.py

--- django/mysite/chat/consumers.py
+++ django/mysite/chat/consumers.py
@@ -4,72 +4,12 @@ import json
 import numpy as np
 from .model import model
 
-
-lstm_model = model.LstmModel()
-
-
-zeros_list = [[0,0,0]]*21
-
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        # self.count = 0;
-        # seq2seq model instance 생성
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        print(self.scope)
-        self.room_group_name = 'chat_%s' % self.room_name
-        
-        self.model = int
-        # model upload
-        # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
-        
-
-    async def disconnect(self, close_code):
-        # Leave room group
-        # seq2seq model 제거 (del model)
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
-    async def receive(self, text_data):  
-        # 수신받은 json data의 전처리이후 모델에 입력하여 text출력
-        # 속도에 따라서 받을 때 마다가 아닌 일정 간격을 모델에 입력해야 할 수도 있음
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        print("receive {0}".format(message) )
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    async def chat_message(self, event):
-        # text를 다시 json파일로 변경 후 송신
-        message = event['message']
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
-
-
-
 class webCamConsumers(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.count = 0
+        self.self.lstm_model = model.LstmModel()
+        self.zeros_list = [[0,0,0]]*21
     async def connect(self):
         self.room_group_name = "TestRoom"
         
@@ -78,7 +18,6 @@ class webCamConsumers(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-        # print(self.channel_name)
         await self.accept()
         self.frame_dict = dict()
         self.frame_dict[self.channel_name]=[]
@@ -110,7 +49,7 @@ class webCamConsumers(AsyncWebsocketConsumer):
             )
 
             self.frame_dict[self.channel_name].clear()
-            self.count = 0;
+            self.count = 0
             return
 
         result = self.preprocess(receive_dict)
@@ -131,8 +70,6 @@ class webCamConsumers(AsyncWebsocketConsumer):
 
         if len(data.keys()) < 3:
             return None
-
-
 
         tmp_list = []
         for data_ in data['landmarks']:
@@ -159,32 +96,22 @@ class webCamConsumers(AsyncWebsocketConsumer):
             tmp_list = first
 
 
-        elif len(tmp_list) == 1: # 1개의 손만 인식된 경우 
-            tmp_zeros = zeros_list.copy()
+        elif len(tmp_list) == 1: 
+            tmp_zeros = self.zeros_list.copy()
             tmp_list = tmp_list[0]
             if (data['handClass'][0]['label'] == 'Right'):
                 tmp_list.extend(tmp_zeros)
-                # print('left')
+
             else :                
                 tmp_zeros.extend(tmp_list)
                 tmp_list = tmp_zeros
-                # print('right')
 
-
-        print(self.count)
-        self.count +=1
-
-        print('len ',len(tmp_list))# 42가 출력되어야함
 
         return tmp_list
 
     def predict(self,data):
         datas = np.array(data)
-
-        predict_word = lstm_model.predictWord(datas)
-
-        print('predict',datas.shape)
-        predict = str(datas.shape)
-        return predict
-
-
+        predict_word = self.lstm_model.predictWord(datas)
+        print('input shape: ',datas.shape,' predict: ',predict_word)
+        return predict_word
+ 
